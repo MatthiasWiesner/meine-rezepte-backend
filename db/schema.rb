8f7ccb66d2d9032ca9499d85f1870e57e9120d29
Matthias Wiesner <matthias.wiesner@googlemail.com>
2018-09-13 11:41:01 +0200

    change_recipes_title_index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_154538) do
+ActiveRecord::Schema.define(version: 2018_09_13_093608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_154538) do
     t.string "pictureList", default: [], array: true
     t.integer "updated_by"
     t.index ["organization_id"], name: "index_recipes_on_organization_id"
-    t.index ["title"], name: "index_recipes_on_title", unique: true
+    t.index ["title", "organization_id"], name: "index_recipes_on_title_and_organization_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
